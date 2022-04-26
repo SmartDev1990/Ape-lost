b31bad88d92520aa93c145c1c11505ed3a32c19b
nanda yusra <nandayusra100@gmail.com>
2022-04-27 00:14:15 +0700

    container Fix

--- components/Feature.js
+++ components/Feature.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="section position-relative" background-color="#10083B" style={{marginTop: "30px", marginBottom: "30px"}}>
       <Container>
       <h3 className="feature-content2">A CILIVIZATION AUGMENTED</h3>
-      <div className="cardcontainer">
+      <div className="cardcontainer2">
       <div className="wrapper">
         <div className="wrappercard">
           <img className="wrappericon" src="/images/NFT1.png" />

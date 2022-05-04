88fba8cad07c2efc524b43d3c24119617a876bc4
nanda yusra <nandayusra100@gmail.com>
2022-05-05 00:02:11 +0700

    fix file

--- components/Apemap.js
+++ components/Apemap.js
@@ -4,13 +4,11 @@ import { Container, Row, Col } from 'reactstrap';
 const Hero = () => {
   return (
     <section className="section" style={{marginTop: "100px", marginBottom: "100px"}} id="APEMAP">
-      <div className="container">
       <h3 className="feature-content2">THE APE MAP</h3>
       <div className="containerApe">
       <div className="feature-header">
           <img className="Mapicon" src="/images/APEMAP.png" style={{alignItems: "center"}}/>
           </div>
-        </div>
        </div>
     </section>
   );
